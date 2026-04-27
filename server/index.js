24142c75ba959f623683ed696a3c366e6bcd87ef
pazamorta <juan.trujillopez@gmail.com>
2026-04-27 14:13:35 +0200

    fix: rename Legal_Name__c to GTCX_Legal_Name__c to match Salesforce schema

--- server/index.js
+++ server/index.js
@@ -743,7 +743,7 @@ app.post('/api/salesforce/invoice', async (req, res) => {
                  BillingStreet: '203 Eversholt Street',
                  BillingCity: 'London',
                  BillingPostalCode: 'NW1 1BU.',
-                 Legal_Name__c: data.companyName,
+                 GTCX_Legal_Name__c: data.companyName,
                  GTCX_Points_Of_Delivery_Count__c: data.sites?.length || 0,
                  GTCX_Broker_Name__c: 'Easy Broker',
                  GTCX_Credit_Score__c: 75
